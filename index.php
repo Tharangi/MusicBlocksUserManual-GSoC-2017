d1a4681de45c69d4cbddeba6ee652e1c7c6ff6fc
Tharangi <tharangidinuka@gmail.com>
2017-07-03 03:19:46 +0800

    quick start page

--- index.php
+++ index.php
@@ -221,8 +221,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"">
     <title>Blocks User Manual</title>
 
     <link href="./vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
