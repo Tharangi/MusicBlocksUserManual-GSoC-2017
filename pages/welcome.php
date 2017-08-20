f43949709bc5e02888b83f0109c4554d2a366d6f
Dinuka Tharangi <Tharangi@users.noreply.github.com>
2017-08-20 11:44:41 +0530

    Typo corrections and styling Introduction page

--- pages/welcome.php
+++ pages/welcome.php
@@ -4,18 +4,18 @@
     <div class="jumbotron">
         <div class="container">
             <h1><center>Welcome to Music Blocks</center></h1>
-            <p>Music Blocks, a collection of programming tools for exploring fundamental musical concepts in an integrative and fun way.</p>
+            <h3><small><center>~ A collection of programming tools for exploring fundamental musical concepts in an interactive and fun way ~</center><small></h3>
             <p>
             <center>
-                <a class="btn btn-primary btn-lg" href="https://codeload.github.com/walterbender/musicblocks/zip/master" target="_blank" role="button" align="left">Download Music Blocks</a>
-                <a class="btn btn-primary btn-lg" href="<?php echo $PATH_PREFIX ?>page=download_pdf" id="download_pdf" role="button" align="right">
+                <a class="btn btn-primary active btn-lg" href="https://codeload.github.com/walterbender/musicblocks/zip/master" target="_blank" role="button" align="left">Download Music Blocks</a>
+                <a class="btn btn-primary active btn-lg" href="<?php echo $PATH_PREFIX ?>page=download_pdf" id="download_pdf" role="button" align="right" target="_blank">
                     Download User Guide
                 </a>
             </center>
             </p>
             <p>
             <center>
-                <a class="btn btn-success btn-lg" href="http://walterbender.github.io/musicblocks/" target="_blank" role="button" align="left">Play Music Blocks Now!</a>
+                <a class="btn btn-success active btn-lg" href="http://walterbender.github.io/musicblocks/" target="_blank" role="button" align="left">Play Music Blocks Now!</a>
             </center>
             </p>
             <p>
