d04364a7512856e60a08cb3bca85d15edb58e860
Tharangi <tharangidinuka@gmail.com>
2017-08-22 00:07:47 +0800

    css changes

--- pages/welcome.php
+++ pages/welcome.php
@@ -1,30 +1,28 @@
 <section id="welcome-page">
     <h1 class="page-header">Welcome</h1>
-    <br/>
     <div class="jumbotron">
         <div class="container">
             <h1><center>Welcome to Music Blocks</center></h1>
             <h3><small><center>~ A collection of programming tools for exploring fundamental musical concepts in an interactive and fun way ~</center><small></h3>
             <p>
-            <center>
-                <a class="btn btn-primary active btn-lg" href="https://codeload.github.com/walterbender/musicblocks/zip/master" target="_blank" role="button" align="left">Download Music Blocks</a>
-                <a class="btn btn-primary active btn-lg" href="<?php echo $PATH_PREFIX ?>page=download_pdf" id="download_pdf" role="button" align="right" target="_blank">
-                    Download User Guide
-                </a>
-            </center>
+                <center>
+                    <a class="btn btn-primary active btn-lg" href="https://codeload.github.com/walterbender/musicblocks/zip/master" target="_blank" role="button" align="left">Download Music Blocks</a>
+                    <a class="btn btn-primary active btn-lg" href="<?php echo $PATH_PREFIX ?>page=download_pdf" id="download_pdf" role="button" align="right" target="_blank">
+                        Download User Guide
+                    </a>
+                </center>
             </p>
             <p>
-            <center>
-                <a class="btn btn-success active btn-lg" href="http://walterbender.github.io/musicblocks/" target="_blank" role="button" align="left">Play Music Blocks Now!</a>
-            </center>
+                <center>
+                    <a class="btn btn-success active btn-lg" href="http://walterbender.github.io/musicblocks/" target="_blank" role="button" align="left">Play Music Blocks Now!</a>
+                </center>
             </p>
             <p>
-            <center>
-                <img src="./dist/img/Welcome/github.png" /><a class="btn btn-link" href="https://github.com/walterbender/musicblocks" target="_blank" role="button" align="left">  Contribute to Music Blocks</a>
-            </center>
+                <center>
+                    <img src="./dist/img/Welcome/github.png" /><a class="btn btn-link" href="https://github.com/walterbender/musicblocks" target="_blank" role="button" align="left">  Contribute to Music Blocks</a>
+                </center>
             </p>
-            <br>
-            <br>
+            <br/>
             <p align="right"><a class="btn btn-link" href="http://chat.sugarlabs.org:9090/" target="_blank" role="button" align="left">Join Sugar Labs conversations</a></p>
         </div>
     </div>
